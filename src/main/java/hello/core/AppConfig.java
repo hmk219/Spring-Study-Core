aaa90f2cdc3022412d56148da523e40b7f30ccf2
hmk219 <hmkreator219@gmail.com>
2021-09-09 04:49:24 +0900

    AppConfig refactoring

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,6 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -10,15 +11,15 @@ import hello.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
-/*    MemoryMemberRepository의 객체를 생성하고
-    -> MemberServiceImpl 객체를 생성하면서 생성자의 참조값으로 전달 (생성자 주입)
-    => MemberServiceImpl에 MemoryMemberRepository 객체의 의존관계 주입 */
-
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 /*    MemoryMemberRepository와 FixDiscountPolicy 객체를 생성하고
     -> OrderServiceImpl 객체를 생성하면서 생성자의 참조값으로 전달 (생성자 주입)
