ae788a2fc345a4030006a1a391b383645180efc0
hmk219 <hmkreator219@gmail.com>
2021-09-12 18:26:05 +0900

    @Autowired 옵션

---
+++ src/test/java/hello/core/autowired/AutowiredTest.java
@@ -0,0 +1,41 @@
+package hello.core.autowired;
+
+import hello.core.member.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.lang.Nullable;
+
+import java.util.Optional;
+
+public class AutowiredTest {
+
+    @Test
+    void AutowiredOption() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class);
+    }
+
+    static class TestBean {
+
+//        Member는 스프링 빈이 아님
+
+        //호출 안됨
+        @Autowired(required = false) // 기본값은 true
+        public void setNoBean1(Member member) {
+            System.out.println("setNoBean1 = " + member);
+        }
+
+        //null 호출
+        @Autowired
+        public void setNoBean2(@Nullable Member member) {
+            System.out.println("setNoBean2 = " + member);
+        }
+
+        //Optional.empty 호출
+        @Autowired(required = false)
+        public void setNoBean3(Optional<Member> member) {
+            System.out.println("setNoBean3 = " + member);
+        }
+    }
+}
